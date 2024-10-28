d8eded6f6cab73621e2b3e2b69e30ee0e4db57ff
Ashhhhhh520 <714898018@qq.com>
2024-10-28 22:25:50 +0800

    update

--- src/identity_test/BlazorWasmTest/Program.cs
+++ src/identity_test/BlazorWasmTest/Program.cs
@@ -8,4 +8,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddOidcAuthentication(opts =>
+{
+    opts.ProviderOptions.Authority = "http://localhost:5000";
+    opts.ProviderOptions.ClientId = "blazor_wasm_test";
+    opts.ProviderOptions.ResponseType = "token";
+
+    opts.AuthenticationPaths.LogInCallbackPath = "/oidc/callback";
+});
+
 await builder.Build().RunAsync();
