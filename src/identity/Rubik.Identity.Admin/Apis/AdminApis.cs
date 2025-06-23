9901319fd4bca979cb5e907a524432b5acd0fc8d
Ashhhhhh520 <714898018@qq.com>
2025-06-23 23:14:57 +0800

    update nuget add plsm web porj

--- src/identity/Rubik.Identity.Admin/Apis/AdminApis.cs
+++ src/identity/Rubik.Identity.Admin/Apis/AdminApis.cs
@@ -5,6 +5,13 @@ namespace Rubik.Identity.Admin.Apis
 {
     public class AdminApis
     {
+        /// <summary>
+        /// 获取用户sys应用的菜单/权限
+        /// </summary>
+        /// <param name="freeSql"></param>
+        /// <param name="identity"></param>
+        /// <param name="sys"></param>
+        /// <returns></returns>
         public static async Task<string> UserPermissions([FromServices]IFreeSql freeSql, [FromServices]UserIdentityAccessor identity,string sys)
         {
             // 获取用户的sys下的权限
---
+++ src/plms/Rubik.PLMS.Host/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Rubik.PLMS.Host;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddAntDesign();
+
+
+await builder.Build().RunAsync();
